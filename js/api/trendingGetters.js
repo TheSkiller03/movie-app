91f33b4b2263763866f7eef4bbaa6da97f9af983
TheSkiller03 <theskiller243@gmail.com>
2024-03-07 15:18:21 +0100

    aggiunto gitingnore e .env

--- js/api/trendingGetters.js
+++ js/api/trendingGetters.js
@@ -1,19 +1,11 @@
-import { baseUrl } from "./config.js";
+import { baseUrl,opts } from "./config.js";
 
 //const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
 //const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
 //const url = 'https://api.themoviedb.org/3/trending/tv/day?language=en-US';
 
 
-const opts = {
-    headers: {
-        Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGYyZjg3M2IxOTY2ODZlMGM3ZGI2M2VhMGM0OTIxYyIsInN1YiI6IjY1ZTg2OWU1YTZjMTA0MDE4N2U5ZWNlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GRWRqrLJpSvav7OVtxS2ZUkKVePbODK5T2H9BtAbzV8",
-    },
-};
-
-
-export const getTrending = async (baseUrl) => {
+export const getTrending = async () => {
 
     const response = await fetch(baseUrl + "/trending/all/day?language=en-US", opts);
 
@@ -30,7 +22,7 @@ export const getTrending = async (baseUrl) => {
  * @returns film di tendenza
  */
 
-export const getTrendingMovie = async (baseUrl) =>{
+export const getTrendingMovie = async () =>{
 
     const response = await fetch(baseUrl +"/trending/movie/day?language=en-US", opts);
 
@@ -45,7 +37,7 @@ export const getTrendingMovie = async (baseUrl) =>{
  * @returns serie tv di tenenza
  */
 
-export const getTrendingTv = async(baseUrl) =>{
+export const getTrendingTv = async() =>{
 
     const response = await fetch(baseUrl +"/trending/tv/day?language=en-US", opts);
 
@@ -54,7 +46,7 @@ export const getTrendingTv = async(baseUrl) =>{
     return data;
 }
 
-export const searchPeople = async(baseUrl, person) =>{
+export const searchPeople = async(person) =>{
 
     const response = await fetch(baseUrl +`/search/person?query=${person}`, opts);
 
